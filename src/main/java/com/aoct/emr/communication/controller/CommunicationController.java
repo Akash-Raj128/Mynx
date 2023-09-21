011ca3473d84f4bfcc1d7f53beca608ce3444e91
manishnupt <48364068+manishnupt@users.noreply.github.com>
2023-09-21 17:45:25 +0530

    Feature/login manish (#3)
    * Update application.properties
    
    * whole forgot password flow
    
    * cors removed
    
    * cors for communicate

--- src/main/java/com/aoct/emr/communication/controller/CommunicationController.java
+++ src/main/java/com/aoct/emr/communication/controller/CommunicationController.java
@@ -1,10 +1,12 @@
 package com.aoct.emr.communication.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aoct.emr.communication.bl.CommunicationBl;
@@ -14,6 +16,7 @@ import com.aoct.emr.communication.uiRequest.OtpUiRequest;
 import com.aoct.emr.communication.uiRequest.OtpValidationRequest;
 
 @RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 @RequestMapping("/communicate")
 public class CommunicationController {
 
