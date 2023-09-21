580ecf93017cae965fb6146e90a94f96ecf7b949
Prince Anand <59204771+prince0602@users.noreply.github.com>
2023-09-21 20:49:00 +0530

    Update SecurityConfig.java

--- src/main/java/com/aoct/emr/common/config/SecurityConfig.java
+++ src/main/java/com/aoct/emr/common/config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
 	    http
 	        .csrf(csrf -> csrf.disable())
 	        .authorizeHttpRequests(auth -> auth
-	            .requestMatchers("/auth/login").permitAll() 
+	            .requestMatchers("/auth/login").permitAll()
+		    .requestMatchers("").permitAll()	       
 	            .requestMatchers("/api/**").authenticated()
 	            .anyRequest().authenticated()
 	        )
