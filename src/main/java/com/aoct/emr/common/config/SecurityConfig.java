1bff4201de0a2eda93afa0c163f2fbb2f4215488
Prince Anand <59204771+prince0602@users.noreply.github.com>
2023-09-21 20:58:03 +0530

    Feature/login manish (#4)
    * Update application.properties
    
    * whole forgot password flow
    
    * cors removed
    
    * cors for communicate
    
    * Update SecurityConfig.java
    
    * Update SecurityConfig.java
    
    ---------
    
    Co-authored-by: manishnupt <48364068+manishnupt@users.noreply.github.com>
    Co-authored-by: Manishnupt <manish.nupt@gmail.com>

--- src/main/java/com/aoct/emr/common/config/SecurityConfig.java
+++ src/main/java/com/aoct/emr/common/config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
 	    http
 	        .csrf(csrf -> csrf.disable())
 	        .authorizeHttpRequests(auth -> auth
-	            .requestMatchers("/auth/login").permitAll() 
+	            .requestMatchers("/auth/**").permitAll()
+		    .requestMatchers("/communicate/**").permitAll()	       
 	            .requestMatchers("/api/**").authenticated()
 	            .anyRequest().authenticated()
 	        )
